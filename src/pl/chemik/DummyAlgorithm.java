d7dbb00c787f8d1467575ab4d5c7a1f993be05ef
Chemik <dj-chemik@wp.pl>
2020-11-02 23:49:48 +0100

    Generate Dummy Algorithm Results

---
+++ src/pl/chemik/DummyAlgorithm.java
@@ -0,0 +1,24 @@
+package pl.chemik;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DummyAlgorithm {
+    public void generateSolutions() {
+        for (int size = 50; size <= 500; size+=50) {
+            String filename = "filesOutputDummyAlgorithm/dummy_" + size + ".txt";
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(filename);
+                writer.write(size + "\n" );
+                for (int i = 1; i <= size; i++) {
+                    writer.write(i + " " );
+                }
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
